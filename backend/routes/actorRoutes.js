46f84c9429a0406753c584f63f0aab74f30838e1
Alvin0305 <alvinanildas@gmail.com>
2025-05-10 20:39:18 +0530

    image upload

--- backend/routes/actorRoutes.js
+++ backend/routes/actorRoutes.js
@@ -2,13 +2,18 @@ const express = require("express");
 const router = express.Router();
 const actorController = require("../controllers/actorController");
 const upload = require("../middlewares/upload");
-const parseFormData = require("../middlewares/upload");
+const { parseFormData } = require("../middlewares/upload");
 
 router.get("/", actorController.getAllActors);
 router.get("/search", actorController.searchActor);
 router.get("/:id", actorController.getActorById);
 router.get("/:id/movies", actorController.getActorMovies);
-router.post("/", upload.single("image"), parseFormData, actorController.createActor);
+router.post(
+  "/",
+  upload.single("image"),
+  parseFormData,
+  actorController.createActor
+);
 router.put("/:id", actorController.updateActor);
 router.delete("/:id", actorController.deleteActor);
 
