839933593ea0fa564585ec1342a592d2306073de
Alvin0305 <alvinanildas@gmail.com>
2025-05-10 15:40:31 +0530

    deploy test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,19 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const authRoutes = require("./authRoutes");
-// const movieRoutes = require("./movieRoutes");
-// const actorRoutes = require("./actorRoutes");
-// const genreRoutes = require("./genreRoutes");
-// const platformRoutes = require("./platformRoutes");
-// const reviewRoutes = require("./reviewRoutes");
-// const userRoutes = require("./userRoutes");
+const movieRoutes = require("./movieRoutes");
+const actorRoutes = require("./actorRoutes");
+const genreRoutes = require("./genreRoutes");
+const platformRoutes = require("./platformRoutes");
+const reviewRoutes = require("./reviewRoutes");
+const userRoutes = require("./userRoutes");
 
 router.use("/auth", authRoutes);
-// router.use("/movies", movieRoutes);
-// router.use("/actors", actorRoutes);
-// router.use("/genres", genreRoutes);
-// router.use("/platforms", platformRoutes);
-// router.use("/reviews", reviewRoutes);
-// router.use("/users", userRoutes);
+router.use("/movies", movieRoutes);
+router.use("/actors", actorRoutes);
+router.use("/genres", genreRoutes);
+router.use("/platforms", platformRoutes);
+router.use("/reviews", reviewRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
